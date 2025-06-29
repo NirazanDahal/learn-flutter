b9d1134390e0c2a7ed5d9f159f2b63b11e99e5eb
NirazanDahal <nirajandahal54@gmail.com>
2025-06-29 08:13:31 +0545

    started learning flutter

---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+class Constants {
+
+String myName="Nirajan Dahal";
+int myPhoneNumber=9898989898;
+double myBankBalance=00.00;
+bool someThing=false;
+List<String> stringList=["List1","List2"];
+List<Map<String,dynamic>> objectList=[{
+  "Name":"Nirajan Dahal",
+  "Roll No":20,
+  "something":false
+},
+{"Name":"Nirajan Dahal",
+  "Roll No":20,
+  "something":false}
+  ];
+
+  final String name="date";
+  late String anotherName;
+ static const String anotherAnotherName="Nirajan";
+}
